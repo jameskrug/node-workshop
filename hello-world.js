dc7b57da21959924fb5278f6e8d50c5738275eb0
James <krugger322@hotmail.com>
2016-10-10 17:37:06 +0000

    added function to write hello world every second

--- hello-world.js
+++ hello-world.js
@@ -1,4 +1,17 @@
+/*
+
 console.log("hello world");
 setTimeout(function(){
     console.log("hello world again!")
 }, 10000);
+
+*/
+
+function unlimitedHelloWorlds(){
+    setTimeout(function(){
+        console.log("hello world");
+        unlimitedHelloWorlds();
+    }, 1000);
+}
+
+unlimitedHelloWorlds();
